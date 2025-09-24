03abac655f3444c4d95ee2fbe66020c8a2786884
MaxBlair123 <maxwelltblair@gmail.com>
2025-09-24 18:33:57 +1000

    Just familiarising myself with merging

--- start-server.bat
+++ start-server.bat
@@ -1,5 +1,3 @@
 @echo off
-set PATH=%PATH%;%CD%\nodejs-portable\node-v20.18.0-win-x64
-set DATABASE_URL=file:./dev.db
-npm run dev
-pause
+set DATABASE_URL=file:./data.ms/data.mdb
+.\node_modules\.bin\next.cmd dev --turbopack
